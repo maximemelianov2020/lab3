1e5b0665d01c1c335affef4ee3caeda507af0a71
maximemelianov2020 <maximemelianov2020@gmail.com>
2025-12-10 08:52:48 +0300

    Update Main.java

--- src/main/java/lab3/Main.java
+++ src/main/java/lab3/Main.java
@@ -1,4 +1,20 @@
 package lab3;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Главный класс для запуска сравнения производительности коллекций.
+ */
 public class Main {
+    public static void main(String[] args) {
+        int iterations = 2000;
+
+        List<ListPerformanceResult> results = new ArrayList<>();
+        results.addAll(ListPerformanceTester.testListPerformance("ArrayList", new ArrayList<>(), iterations));
+        results.addAll(ListPerformanceTester.testListPerformance("LinkedList", new LinkedList<>(), iterations));
+
+        ListPerformanceTester.printResultsTable(results);
+    }
 }
